cf0f67bc12e20595af1fa96001a8d9ac2e094b7a
TheDanishMexican <danieljchristjansen@gmail.com>
2024-02-12 11:01:08 +0100

    added getPerson(id) route)

--- src/main/java/kea/exercise/person/controller/PersonController.java
+++ src/main/java/kea/exercise/person/controller/PersonController.java
@@ -4,9 +4,12 @@ import kea.exercise.person.model.Person;
 import kea.exercise.person.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PersonController {
@@ -15,11 +18,17 @@ public class PersonController {
     private PersonRepository personRepository;
 
     @GetMapping("/persons")
- public List<Person> getAllPersons() {
+    public List<Person> getAllPersons() {
 
     List<Person> persons = personRepository.findAll();
 
      return persons;
  }
+
+    @GetMapping("/persons/{id}")
+    public Person getPerson(@PathVariable int id) {
+        Optional<Person> person = personRepository.findById(id);
+        return person.orElse(null);
+    }
 
 }
